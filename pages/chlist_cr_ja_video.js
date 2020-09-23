4fe7fb85ea96859ad6679cde48bf4b24d555bdee
eriosonoe <eri.osonoe@smartnews.com>
2020-09-23 11:56:58 +0900

    font chenge

--- pages/chlist_cr_ja_video.js
+++ pages/chlist_cr_ja_video.js
@@ -235,6 +235,11 @@ export default class App extends React.Component {
 					body * {
 						font-size:16px;
 					}
+
+					body{
+						font-family:-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+					}
+
 				`}</style>
 			</>
 		);
